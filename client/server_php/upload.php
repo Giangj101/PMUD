3afbc7aed2e64ddc1b09c14bd158de3bf96c1a61
Giang <96253358+Giangj101@users.noreply.github.com>
2023-01-08 11:34:24 +0700

    gg
    day nhe

---
+++ client/server_php/upload.php
@@ -0,0 +1,11 @@
+<?php 
+$base64Image  = $_POST['img'];
+$imageData = base64_decode(preg_replace('#^data:image/\w+;base64,#i', '', $base64Image));
+$randomNumber = mt_rand(1, 10);
+$string = strval($randomNumber);
+file_put_contents('../image/img_' .$string. '.png', $imageData);
+
+
+
+
+?>
